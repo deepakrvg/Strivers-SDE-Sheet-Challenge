3bca1c785659c3b028f10e292c0991a7758df654
Deepak Gupta <76244467+deepakrvg@users.noreply.github.com>
2023-06-13 01:51:30 +0530

    Update Set Matrix Zeros.cpp

--- Arrays/Set Matrix Zeros.cpp
+++ Arrays/Set Matrix Zeros.cpp
@@ -5,31 +5,38 @@ void setZeros(vector<vector<int>> &matrix)
 	// Write your code here.
 	int n = matrix.size();
 	int m = matrix[0].size();
-	vector<vector<int>> mat(n, vector<int>(m, -1));
-	
-	unordered_map<int, int> rows;
-	unordered_map<int, int> cols;
-
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (matrix[i][j] == 0) mat[i][j] = 0;
-		}
-	}
+	int col = 1;
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			if (matrix[i][j] == 0) {
-				rows[i]++;
-				cols[j]++;
+				matrix[i][0] = 0;
+				if (j == 0) {
+					col = 0;
+				}
+				else {
+					matrix[0][j] = 0;
+				}
 			}
 		}
 	}
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (rows[i] || cols[j]) {
+	for (int i = 1; i < n; i++) {
+		for (int j = 1; j < m; j++) {
+			if (matrix[i][0] == 0 || matrix[0][j] == 0) {
 				matrix[i][j] = 0;
 			}
+		}
+	}
+
+	for (int i = 1; i < m; i++) {
+		if (matrix[0][0] == 0) {
+			matrix[0][i] = 0;
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		if (col == 0) {
+			matrix[i][0] = 0;
 		}
 	}
 }
