201eda5891009419cedbd2a73a855e687d5682af
Deepak Gupta <76244467+deepakrvg@users.noreply.github.com>
2023-07-23 00:24:42 +0530

    Update Clone Linked List with Random and Next Pointer.cpp

--- Linked List and Arrays/Clone Linked List with Random and Next Pointer.cpp
+++ Linked List and Arrays/Clone Linked List with Random and Next Pointer.cpp
@@ -20,63 +20,34 @@ public:
         if (head == NULL) {
             return NULL;
         }
-        Node *l1 = NULL;
-        Node *l2 = head;
-        Node *temp;
-        int len = 0;
-        while (l2 != NULL) {
-            if (l1 == NULL) {
-                l1 = new Node(l2->val);
-                temp = l1;
+        Node *curr = head;
+        while (curr != NULL) {
+            Node *temp = curr->next;
+            curr->next = new Node(curr->val);
+            curr->next->next = temp;
+            curr = temp;
+        }
+        curr = head;
+
+        while (curr != NULL) {
+            if (curr->next != NULL) {
+                curr->next->random = (curr->random != NULL) ? curr->random->next : NULL;
             }
-            else {
-                Node *new_node = new Node(l2->val);
-                temp->next = new_node;
-                temp = new_node;
-            }
-            l2 = l2->next;
-            len++;
+            curr = curr->next->next;
         }
 
-        int arr[len];
-        memset(arr, 0, sizeof(arr));
-        l2 = head;
-        Node* l3 = head;
-        int index = 0;
-        int k = 0;
-        while (l2 != NULL) {
-            l3 = head;
-            Node *temp = l2->random;
-            index = 0;
-            while (l3 != temp) {
-                l3 = l3->next;
-                index++;
+        curr = head;
+        Node *l1 = head->next;
+        Node *l2 = l1;
+        while (curr != NULL) {
+            curr->next = curr->next->next;
+            if (l1->next == NULL) {
+                break;
             }
-            arr[k] = index;
-            l2 = l2->next;
-            k++;
+            l1->next = l1->next->next;
+            curr = curr->next;
+            l1 = l1->next;
         }
-
-        l2 = l1;
-        l3 = l1;
-        k = 0;
-        while (l2 != NULL) {
-            l3 = l1;
-            if (arr[k] == len) {
-                k++;
-                l2->random = NULL;
-                l2 = l2->next;
-                continue;
-            }
-            while (arr[k]) {
-                l3 = l3->next;
-                arr[k]--;
-            }
-            k++;
-            l2->random = l3;
-            l2 = l2->next;
-        }
-
-        return l1;
+        return l2;
     }
 };
